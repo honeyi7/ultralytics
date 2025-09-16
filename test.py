4d6c9b303c23ad78b6dc20d76c078f661ff553b9
honeyi7 <zhangzhenting05@gmail.com>
2025-09-16 18:19:12 +0800

    7th test

--- test.py
+++ test.py
@@ -30,6 +30,7 @@ def main():
     print(" 视频打开成功，开始处理...")
     print("111")
     print("2222")
+    print("333")
     # 获取视频的宽度、高度和FPS
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
